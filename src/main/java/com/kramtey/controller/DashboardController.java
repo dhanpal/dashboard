8eb0921c3c1e86b020554b73d29e125b0fd3ae9a
Dhanpal Chauhan <dhanpalchauhan@Dhanpals-MacBook-Pro.local>
2018-04-08 13:00:53 +0200

    not working

--- src/main/java/com/kramtey/controller/DashboardController.java
+++ src/main/java/com/kramtey/controller/DashboardController.java
@@ -1,28 +1,15 @@
 package com.kramtey.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.kramtey.repository.DashboardRepository;
 
 @RestController
 @RequestMapping("")
 public class DashboardController {
-	@Autowired
-	DashboardRepository dashboardRepository;
 
 	@GetMapping("/")
-	String getAllDashboards(ModelMap modal) {
-		modal.addAttribute("title","Kramtey Solutions");
-		return "index";
-	}
-
-	@GetMapping("/partials/{page}")
-	String partialHandler(@PathVariable("page") final String page) {
-		return page;
+	String getAllDashboards() {
+		return "Kramtey Solutions";
 	}
 }
